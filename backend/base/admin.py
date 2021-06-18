38c935e53f3fa94b2ee513a6e4cc662c2b51c67a
Ankit Goyal <ankit80570@gmail.com>
2021-06-18 13:03:30 +0530

    Registered on admin site

--- backend/base/admin.py
+++ backend/base/admin.py
@@ -2,6 +2,12 @@ from django.contrib import admin
 from .models import *
 # Register your models here.
 
-@admin.register(user)
-class user(admin.ModelAdmin):
-    list_display = ['id', 'email']
+# @admin.register(user)
+# class user(admin.ModelAdmin):
+#     list_display = ['id', 'email']
+admin.site.register(user)
+admin.site.register(course)
+admin.site.register(feedback)
+admin.site.register(video)
+admin.site.register(skill)
+admin.site.register(notes)
